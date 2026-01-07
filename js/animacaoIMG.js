857d50d1e5a85d1b9209ddef63a3833f55fb9ea2
RickFerreira <richard.ferreira.ext@synchro.com.br>
2026-01-07 14:41:42 -0300

    refactor: reformula todo o visual do site

--- js/animacaoIMG.js
+++
@@ -1,9 +0,0 @@
-function animacaoIMG() {
-    document.querySelectorAll('img').forEach((img, index) => {
-        if(img.getBoundingClientRect().top < window.innerHeight){
-            img.src = img.getAttribute('data-src');
-        }
-    })
-}
-
-window.addEventListener('scroll', animacaoIMG);
